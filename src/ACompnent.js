78c35183f668960dd7ce473d60efa03f2d132f42
Shorn Jacob <shorn@transparency.consulting>
2019-03-06 16:58:18 +1300

    Tested a reactstrap element

--- src/ACompnent.js
+++ src/ACompnent.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BComponent from './BCompnent'
+import Button from 'react-bootstrap/Button';
 
 export default function ACompnent() {
   return (
     <div>
       <BComponent />
+      <Button />
     </div>
   )
 }
--- src/AComponent.test.js
+++ src/AComponent.test.js
@@ -3,10 +3,20 @@ import { shallow } from 'enzyme';
 import BComponent from './BCompnent'
 import AComponent from './ACompnent';
 import {expect} from 'chai'
+import Button from 'react-bootstrap/Button';
+
 
 describe('<AComponent />', () => {
     it('renders BComponent', () => {
       const wrapper = shallow(<AComponent />);
       expect(wrapper.find(BComponent)).to.have.lengthOf(1);
+    });
+})
+
+
+describe('<Button />', () => {
+    it('renders Button', () => {
+      const wrapper = shallow(<AComponent />);
+      expect(wrapper.find(Button)).to.have.lengthOf(1);
     });
 })
